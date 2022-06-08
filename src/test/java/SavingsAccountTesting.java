1d1d0f0eae857af2a756615533ca2451a40844de
Jayant Sachdeva <jayant.sachdeva@sbi.co.in>
2022-06-08 13:20:28 +0530

    Added Credit Testing

--- src/test/java/SavingsAccountTesting.java
+++ src/test/java/SavingsAccountTesting.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -34,12 +36,45 @@ public class SavingsAccountTesting {
 	}
 	
 	@Test
-	public void extractionOfSavingsAccount() {
+	public void extractionOfSavingsAccountTesting() {
 		Assertions.assertTrue(emf!=null);
 		Assertions.assertTrue(em!=null);
 		SavingsAccount sa = em.find(SavingsAccount.class,102);
 		Assertions.assertTrue(sa!=null);
 		System.out.println("Naam:"+sa.getAccHolder()+" Ac No:"+sa.getAccNo()+" Balancewa:"+sa.getBalance());
+	}
+	
+	@Test
+	public void creditTesting() {
+		Assertions.assertTrue(emf!=null);
+		Assertions.assertTrue(em!=null);
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter Account Number:");
+		int searchAccNumber = s.nextInt();
+		
+		
+		EntityTransaction trxn = em.getTransaction();
+		trxn.begin();
+		SavingsAccount theAccount = em.find(SavingsAccount.class,searchAccNumber);
+		
+		Assertions.assertTrue(theAccount!=null);
+		
+		System.out.println("Naam:"+theAccount.getAccHolder()+" Ac No:"+theAccount.getAccNo()+" Balancewa:"+theAccount.getBalance());
+		
+		Scanner s2 = new Scanner(System.in);
+		System.out.println("Enter Account Number:");
+		int depositBal = s2.nextInt();
+		
+		
+		theAccount.setBalance(theAccount.getBalance()+depositBal);
+		
+		System.out.println("Naam:"+theAccount.getAccHolder()+" Ac No:"+theAccount.getAccNo()+" Balancewa:"+theAccount.getBalance());
+		
+		em.merge(theAccount);
+		trxn.commit();
+		em.close();
+		s.close();
+		s2.close();
 	}
 
 }
